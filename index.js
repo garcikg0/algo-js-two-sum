18906c55f87e7a5d87efd43455972c0e83e9723b
garcikg0 <kg.gar91@gmail.com>
2020-12-24 15:09:42 -0500

    fixed syntax error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const numbers = [2,7,11,15,21]
 
 const twoSum = (numbers, target) => {
     const comp = {};
-    for (let i=0, i < numbers.length; i++) {
+    for (let i=0; i < numbers.length; i++) {
         if (comp[numbers[i]]){
             return [comp[numbers[i]], i]
         }

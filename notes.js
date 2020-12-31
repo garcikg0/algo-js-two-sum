5f5a22750a807caa523369d5798e30100dca0e77
garcikg0 <kg.gar91@gmail.com>
2020-12-31 16:28:23 -0500

    added example explanation

--- notes.js
+++ notes.js
@@ -48,3 +48,35 @@ for (let i=0; i<nums.length; i++){
     }
 }
 
+//FOR EXAMPLE 
+nums = [2,7,11,15] 
+target = 9
+
+const twoSum = (nums, target) => {
+    let dictionary = {}
+    for (let i=0; i<nums.length; i++){
+        let currentNum = nums[i]
+        if (dictionary[currentNum] === 0 || dictionary[currentNum]){
+            return [dictionary[currentNum], i]
+        } else {
+            let match = target - currentNum
+            dictionary[match] = i
+        }
+    }
+};
+
+// First Iteration
+    i= 0
+    currentNum = 2 
+    // 2 does not exist as a key in the dictionary object, so it moves on to the else statement 
+    match = 9 - 2 = 7
+    // save match value as a key (7) and index as value of that key (0)
+    // Therefore, the dictionary object will be updated as follows: 
+    dictionary = {7:0}
+// Second Iteration 
+    i = 1
+    currentNum = 7
+    // 7 exists in dictionary object so we return the value of the key (0) and current iteration (1)
+     
+
+
